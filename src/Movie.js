b6198b036d6bda1f99f10b9476ecc1ff5898eb47
Woojin <sam4woojin@naver.com>
2021-05-11 23:42:51 +0900

    Movie CSS

--- src/Movie.js
+++ src/Movie.js
@@ -5,19 +5,21 @@ import "./Movie.css";
 function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movies__movie">
-      <img src={poster} alt={title} title={title}></img>
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary}</p>
+      <div className="movie__top">
+        <img src={poster} alt={title} title={title}></img>
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+      <p className="movie__summary">{summary}</p>
     </div>
   );
 }
