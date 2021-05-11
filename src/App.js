22103c156e9a59e79daaff97a318000031899ed2
Woojin <sam4woojin@naver.com>
2021-05-11 22:11:04 +0900

    HTML Structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 class App extends React.Component {
   state = {
     isLoading: true,
@@ -26,11 +27,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map((movie) => {
-              // console.log(movie.title);
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => {
               return (
                 <Movie
                   key={movie.id}
@@ -42,7 +46,9 @@ class App extends React.Component {
                 />
               );
             })}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
