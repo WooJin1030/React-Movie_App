305aea801fa9f2eaf69a669fc76e440c07ba3ee6
Woojin <sam4woojin@naver.com>
2021-05-12 18:08:24 +0900

    Show Movie Details

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { HashRouter, Route } from "react-router-dom";
 import "./App.css";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
